2e6f65236a14793585900d7d7e67e64cdda9f466
atharvmunot004 <munot.atharv@gmail.com>
2025-04-09 11:01:42 +0530

    Delete init.py

--- init.py
+++
@@ -1,9 +0,0 @@
-import nltk
-
-# Download necessary NLTK resources
-nltk.download('punkt')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker_tab')
-nltk.download('averaged_perceptron_tagger_eng')
