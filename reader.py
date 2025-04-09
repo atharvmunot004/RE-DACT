a53cf57bd33fec1d9e5d9c9f967231db5be88b6b
atharvmunot004 <munot.atharv@gmail.com>
2025-04-09 11:01:58 +0530

    Delete reader.py

--- reader.py
+++
@@ -1,8 +0,0 @@
-import docx
-import os
-
-for file in os.listdir("input"):
-    if (".doc" in file or ".docx" in file):
-        doc = docx.Document("input/" + file)
-        for para in doc.paragraphs:
-            print(para.text)
